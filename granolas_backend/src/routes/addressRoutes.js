407c7ccf44999f319795e36c04744b6c9bbba3f8
Stenio Vinicios de Medeiros <steniovm@gmail.com>
2022-04-06 19:33:32 -0300

    rotas enderecos

--- granolas_backend/src/routes/addressRoutes.js
+++ granolas_backend/src/routes/addressRoutes.js
@@ -4,9 +4,37 @@ const address = require("../model/address");
 
 const errorHandler = require("../controller/errorHandler");
 
-router.get("/:id", async (req, res) => {
+function dateTime(date) {
+    let dt = date;
+    let dtstring = dt.getFullYear()
+        + '-' + pad(dt.getMonth()+1,2)
+        + '-' + pad(dt.getDate(),2)
+        + 'T' + pad(dt.getHours(),2)
+        + ':' + pad(dt.getMinutes(),2)
+        + ':' + pad(dt.getSeconds(),2);
+    return dtstring.toString();
+}
 
-    const query = `
+function pad(number, length) {
+    let str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+    return str;
+}
+
+router.get("/:id", async (req, res) => {
+    const sql = "SELECT * FROM public.enderecos_usuarios WHERE id=$1"
+    const value = [parseInt(req.params.Id)];    
+
+    await client.query(sql, value,  (err, res) => {
+        if (err) {
+            response.send(err.stack)
+        } else {
+            response.status(200).json(res.rows[0]);
+        }
+    });
+    /*const query = `
         SELECT * FROM
             public.enderecos_usuarios
         LEFT JOIN WHERE id=$1
@@ -23,13 +51,24 @@ router.get("/:id", async (req, res) => {
             console.log(res.rows[0])
         }
 
-    });
+    });*/
 
 });
 
-router.get("/:id", async (req, res) => {});
-
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+    let dates = req.body;
+    console.log(dates);
+    const dt = new Date();
+    
+    let sqlstring = `INSERT INTO public.enderecos_usuarios (id_usuario, cep, numero, complemento, logradouro, cidade, uf, criado_por, criado_em )
+        VALUES ('${dates.iduser}', '${dates.cep}','${dates.numero}', '${dates.complemento}', '${dates.logradouro}', '${dates.cidade}', '${dates.uf}', '${dates.userlog}', ${dateTime(dt)})`;
+    client.query(sqlstring, (err, res) => {
+        console.log(err ? err.stack : res.rows[0])
+        client.end()
+      });
+    res.send(sqlstring);
+    console.log(sqlstring);
+});
 
 router.delete("/:addressId", async (req, res) => {});
 
