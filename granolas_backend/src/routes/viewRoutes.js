a75b25bb9e00df3d76e14e4bec0c6c911fee980f
Jaumelan <49458468+Jaumelan@users.noreply.github.com>
2022-04-07 08:02:37 -0300

    Update viewRoutes.js

--- granolas_backend/src/routes/viewRoutes.js
+++ granolas_backend/src/routes/viewRoutes.js
@@ -1,13 +1,18 @@
 const view = require("../model/view");
 const errorHandler = require("../controller/errorHandler");
+const client = require("../dbConnection");
 
 const router = require("express").Router();
 
 //TODO passar as funções para o controller
 
-router.get("/", async (req, res) => {});
 
-router.get("/:viewId", async (req, res) => {});
+router.get("/", async (req, res) => {
+    let sql = "SELECT * FROM enderecos"
+    await client.query(sql);
+    return res.json(res.rows).status(200)
+
+});
 
 router.post("/", async (req, res) => {});
 
